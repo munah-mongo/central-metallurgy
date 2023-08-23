182615ddbb84dc13f54d97db4d059705ee94edf2
Brayton Anderson <brayton@protonmail.com>
2023-08-23 06:40:01 +0300

    newnow

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -149,7 +149,6 @@ export default makeSource({
     rehypePlugins: [
       rehypeSlug,
       [
-        rehypePrettyCode,
         {
           theme: "github-dark",
           onVisitLine(node) {
