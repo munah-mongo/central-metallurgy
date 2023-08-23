d94c90a382e55e6456654e8411305eb07e9c1108
Brayton Anderson <brayton@protonmail.com>
2023-08-23 06:52:43 +0300

    newy

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,6 +1,5 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import rehypePrettyCode from "rehype-pretty-code"
 import rehypeSlug from "rehype-slug"
 import remarkGfm from "remark-gfm"
 
